70b1f72bb1e34365ce99a52903c0e2da3fdec3dc
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-11 10:33:31 -0300

    :sparkles: Adding swagger users  and posts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,21 @@
-import { IsEmail, IsNotEmpty, IsString, IsBoolean } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ description: 'Email do usuario' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ description: 'Nome completo do usuario' })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'Define se o usuario é administrador',
+    default: false,
+  })
   @IsBoolean()
   admin: boolean;
 }
